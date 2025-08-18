d54654174396d830c72481bd5cfd7fb9a649d93e
iT3E <26583558+iT3E@users.noreply.github.com>
2025-08-17 17:09:08 -0700

    Add player and circleshape classes

--- bootdev-astroids/main.py
+++ bootdev-astroids/main.py
@@ -1,24 +1,24 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(
-        f"""
-    Starting Asteroids!
-    Screen width: {SCREEN_WIDTH}
-    Screen height: {SCREEN_HEIGHT}
-        """
-    )
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen
-        pygame.Surface.fill(screen, color="black")
+
+        screen.fill("black")
+        Player.draw(player, screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
