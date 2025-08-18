8886c88a625667cc91b17618c3189d2ab799925d
iT3E <26583558+iT3E@users.noreply.github.com>
2025-08-17 20:01:44 -0700

    Add movement, add asteroids

--- bootdev-astroids/main.py
+++ bootdev-astroids/main.py
@@ -1,14 +1,24 @@
 import pygame
+import sys
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 
 def main():
     pygame.init()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    group_updatable = pygame.sprite.Group()
+    group_drawable = pygame.sprite.Group()
+    group_asteroids = pygame.sprite.Group()
+    AsteroidField.containers = (group_updatable,)
+    Asteroid.containers = (group_asteroids, group_updatable, group_drawable)
+    Player.containers = (group_updatable, group_drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -16,8 +26,14 @@ def main():
                 return
 
         screen.fill("black")
-        Player.draw(player, screen)
-        Player.update(player, dt)
+        for i in group_drawable:
+            i.draw(screen)
+        group_updatable.update(dt)
+        for i in group_asteroids:
+            if i.col_check(player) == True:
+                return print("Game over!")
+                sys.exit[0]
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
