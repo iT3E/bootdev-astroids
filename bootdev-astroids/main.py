7fa60bc27fbeff58f760d87641bca8d2a770e1c3
iT3E <26583558+iT3E@users.noreply.github.com>
2025-08-18 22:58:48 -0700

    Add score display

--- bootdev-astroids/main.py
+++ bootdev-astroids/main.py
@@ -5,6 +5,7 @@ from player import *
 from asteroid import *
 from asteroidfield import *
 from shot import *
+from score import *
 
 
 def main():
@@ -17,12 +18,14 @@ def main():
     group_drawable = pygame.sprite.Group()
     group_asteroids = pygame.sprite.Group()
     group_shots = pygame.sprite.Group()
+    Score.containers = (group_drawable, group_shots)
     Shot.containers = (group_shots, group_updatable, group_drawable)
     AsteroidField.containers = (group_updatable,)
     Asteroid.containers = (group_asteroids, group_updatable, group_drawable)
     Player.containers = (group_updatable, group_drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
+    game_score_display = Score(100, 100, 2, score)
 
     while True:
         for event in pygame.event.get():
@@ -42,6 +45,7 @@ def main():
                     s.kill()
                     i.split()
                     score = i.score(score)
+                    game_score_display.update_score(score)
                     print(f"Score:{score}")
 
         pygame.display.flip()
---
+++ bootdev-astroids/score.py
@@ -0,0 +1,22 @@
+import pygame
+import random
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS, PLAYER_SHOOT_COOLDOWN, ASTEROID_MIN_RADIUS
+from circleshape import CircleShape
+
+
+class Score(CircleShape):
+    def __init__(self, x, y, radius, initial_score):
+        super().__init__(x, y, radius)
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.default_score = initial_score
+
+    def draw(self, screen):
+        font = pygame.font.SysFont('arial', 30)
+        number_text = font.render(
+            str(self.default_score), True, (255, 255, 255))
+        return screen.blit(number_text, (self.x, self.y))
+
+    def update_score(self, new_score):
+        self.default_score = new_score
